b29ac4caab9fa69c9cce4aec4a08bc1758581214
Remigijus Kiminas <remdex@gmail.com>
2020-08-24 04:52:02 -0400

    correct field name

--- doc/install.sql
+++ doc/install.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `lhc_insult` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `msg_id` bigint(20) NOT NULL,
-  `message` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `msg` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `chat_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `chat_id` (`chat_id`),
