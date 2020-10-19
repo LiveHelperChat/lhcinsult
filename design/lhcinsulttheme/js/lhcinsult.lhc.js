42f496d1bb6bbcd893dd974d0b78e13357869681
Remigijus Kiminas <remdex@gmail.com>
2020-10-19 07:19:29 -0400

    Remove log

--- design/lhcinsulttheme/js/lhcinsult.lhc.js
+++ design/lhcinsulttheme/js/lhcinsult.lhc.js
@@ -1,5 +1,3 @@
-console.log('extension here');
-
 ee.addListener('quoteActionRight', function (params, chat_id, msg_id) {
     var contentOriginal = params['content']();
     params['content'] = function(){ return contentOriginal + ' <br/> <a href="#" id="lhcinsult-msg-'+msg_id+'"><i class="material-icons mr-0">block</i> Mark as insult</a>' }
